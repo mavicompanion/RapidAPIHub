70e2f32cfc4c9f8fdf667a361134fce08340070f
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-26 13:22:57 +0530

    basic functionality works great

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    return render_template('index.html')
 
 @app.route('/weather')
 def get_weather():
@@ -18,7 +18,10 @@ def get_weather():
     return render_template(
         "weather.html",
         title=weather_data["name"],
-        status = weather_data["Weather"][0]["description"].capitalize(),  
+        status = weather_data["weather"][0]["description"].capitalize(),
+        temp=f"{weather_data['main']['temp']:.1f}",
+        feels_like=f"{weather_data['main']['feels_like']:.1f}"
+
     )
 
 if __name__ == "__main__":
